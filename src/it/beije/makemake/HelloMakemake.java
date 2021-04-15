94339f32df08dafb96deb132791458f15fb4b07e
Padawan04 <Padawan04@DESKTOP-KVB7PAD.homenet.telecomitalia.it>
2021-04-15 17:39:25 +0200

    ciao

--- src/it/beije/makemake/HelloMakemake.java
+++ src/it/beije/makemake/HelloMakemake.java
@@ -3,6 +3,7 @@ package it.beije.makemake;
 public class HelloMakemake {
 
 	public static void main(String[] args) {
+		System.out.println("CIAO");
 		System.out.println(
 				"Makemake, ufficialmente designato (136472) Makemake, precedentemente noto con la designazione provvisoria di (136472) 2005 FY9) è il terzo pianeta nano per dimensioni del sistema solare ed è appartenente alla classe dei plutoidi. Il suo diametro è pari all'incirca a 3/4 di quello di Plutone.[9] La sua orbita è per intero situata esternamente rispetto all'orbita di Nettuno, pertanto è corretto definirlo un oggetto transnettuniano, appartenente al gruppo dei cubewani.\n" + 
 				"Ha un satellite conosciuto, S/2015 (136472) 1, la cui scoperta è stata resa nota nell'aprile del 2016[10]. La temperatura estremamente bassa (circa 30 K) che si registra sulla sua superficie fa sì che essa sia ricoperta da ghiacci di metano, etano e probabilmente azoto.[11] Il suo nome richiama la figura di Makemake, divinità della creazione secondo la mitologia pasquense."
