367658132a116b85374b1a5de8f9d49f0af7bd3a
tommicasti <tommi.casti@gmail.com>
2021-04-16 16:33:08 +0200

    esercizi

---
+++ src/it/beije/makemake/esercizi/cicli/figuraAsterischi.java
@@ -0,0 +1,17 @@
+package it.beije.makemake.esercizi.cicli;
+
+import java.util.Scanner;
+
+public class figuraAsterischi {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		for(int i=0; i<n; i++) {
+			for(int j)
+			System.out.println("*");
+		}
+
+	}
+
+}
---
+++ src/it/beije/makemake/esercizi/cicli/primiVenti.java
@@ -0,0 +1,16 @@
+package it.beije.makemake.esercizi.cicli;
+
+import java.util.Scanner;
+
+public class primiVenti {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		for(int i=20; i>=0; i--)
+			System.out.println(i);
+
+	}
+
+	}
+
+
