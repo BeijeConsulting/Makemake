3d49574722c8fc190b3796e0c540f8aca6d89db5
andre <andre@DESKTOP-74HSQGN.wind3.hub>
2021-04-16 15:18:13 +0100

    SET/GET

---
+++ src/it/beije/makemake/SetStr2Var.java
@@ -0,0 +1,18 @@
+package it.beije.makemake;
+
+public class SetStr2Var {
+	
+	public static void main(String[] args) {
+		
+		String s = args[0];
+		
+		String get_name = new String("get");
+		String set_name = new String("set");
+		
+		get_name = get_name + s.toUpperCase().substring(0,1) + s.substring(1,s.length());
+		set_name = set_name + s.toLowerCase().substring(0,1) + s.substring(1,s.length());
+		
+		System.out.println("GET: "+get_name);
+		System.out.println("SET: "+get_name);
+	}	
+}
