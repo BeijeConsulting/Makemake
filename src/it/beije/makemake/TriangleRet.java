f5fee1d8090dca71f30a4851adc2a31dedaf6457
andre <andre@DESKTOP-74HSQGN.wind3.hub>
2021-04-16 15:47:51 +0100

    Stampa Triangolo

---
+++ src/it/beije/makemake/TriangleRet.java
@@ -0,0 +1,14 @@
+package it.beije.makemake;
+
+public class TriangleRet {
+	public static void main(String[] args) {		
+		char ch = '*';
+		char[][] point = new char[10][10];
+		
+		for (int i=0; i<point.length; ++i) {
+			for(int j=0; j<point[i].length; ++j)
+			point[i][j-i] = ch;
+		}
+		System.out.println("Triangolo Rettangolo: "+point);
+	}
+}
