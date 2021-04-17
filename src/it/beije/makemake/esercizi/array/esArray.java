7f7a08bdb31eee5409d42d4e2f0b22960aa748f0
tommicasti <tommi.casti@gmail.com>
2021-04-17 12:03:56 +0200

    esercizi

--- src/it/beije/makemake/esercizi/array/esArray.java
+++ src/it/beije/makemake/esercizi/array/esArray.java
@@ -1,5 +1,7 @@
 package it.beije.makemake.esercizi.array;
 
+import java.util.ArrayList;
+
 public class esArray {
 
 	public static int ultimo(int[] x) {
@@ -66,8 +68,51 @@ public class esArray {
 				cont=cont1;
 		} return cont;
 	}
+	
+	public static double mediaMultipliTre(int[]x) {
+		int cont=0;
+		int somm=0;
+		for(int i=0; i<x.length-1; i++) {
+			if(x[i]%3==0) {
+				cont++;
+				somm+=x[i];
+			}
+		}
+		return (double)somm/cont;
+	}
+	public static void stampaZigZag(int[]x) {
+		for (int i = 0; i < x.length / 2; i++) {
+			System.out.print(x[i] + " " + x[x.length - 1 - i] + " ");
+		}
+		if (x.length % 2 == 1) {
+			System.out.print(x[x.length / 2]);
+		}
+		System.out.println();
+	}
+	 
+	public static double media(int[]x) {
+		int som=0;
+		for(int i=0; i<x.length; i++){
+			som+=x[i];
+			}
+		return (double)som/x.length;
+		}
+	public static String [] addString(String s, String[] a) {
+		String []x = new String[a.length +1];
+		for(int i=0; i<a.length;i++) {
+			x[i]=a[i];
+		}
+		x[a.length]=s;
+	
+		return x;
+	}
+	public static void printArray(String[] p) {
+		for (String str : p) {
+			System.out.print(str + " ");
+		}
+	}
 	public static void main(String[] args) {
-		int [] X= {1,2,3,5,4,6,6,3,3};
+		int [] X= {1,2,3,5,4,6,6,3,3,9};
 		String [] Y= {"ciao","come", "stai"};
 		System.out.println(ultimo(X));
 		System.out.println(indexMax(X));
@@ -75,7 +120,11 @@ public class esArray {
 		System.out.println(containsOb("ciao",Y));
 		System.out.println(isCrescente(X));
 		System.out.println(mostRecurrent(X));
-
+		System.out.println(mediaMultipliTre(X));
+		stampaZigZag(X);
+		System.out.println(media(X));
+		
+		printArray(addString("bello", Y));
 	}
 
 }
