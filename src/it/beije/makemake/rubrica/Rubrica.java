d34c2514e3712e0b4a1926ab1f74edf8274d117c
PierMiglietta <82224786+PierMiglietta@users.noreply.github.com>
2021-04-20 15:07:26 +0200

    Merge,ricerca tramite nome e cognome

--- src/it/beije/makemake/rubrica/Rubrica.java
+++ src/it/beije/makemake/rubrica/Rubrica.java
@@ -6,17 +6,21 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
 
 public class Rubrica {
 	static List<Contatto> contatti=new ArrayList<Contatto>();
 	
 	public static void main(String[] args ) throws Exception{
 		
-		
 		caricaRubrica(contatti,"C:\\Users\\Padawan02\\Desktop\\rubrica1.csv");
-	
+		//mergeRubriche("C:\\Users\\Padawan02\\Desktop\\rubrica1.csv","C:\\Users\\Padawan02\\Desktop\\rubrica2.csv","C:\\Users\\Padawan02\\Desktop\\rubrica_nuova.txt");
+		//nameOrder(contatti);
+		cognomeOrder(contatti);
 	}
+	
 		
 	public static void caricaRubrica(List<Contatto> contatti , String pathFile) throws Exception {
 		
@@ -33,12 +37,59 @@ public class Rubrica {
 			contatto.setEmail(rowParts[3]);
 			contatti.add(contatto);
 		}
-			
-		for(Contatto c : contatti) {
-			System.out.println(c.toString());
-		}
+		
+		//for(Contatto c : contatti) {
+		//	System.out.println(c.toString());
+		//}
+		
 		bufferedReader.close();
 	}
 	
 	
+	public static void mergeRubriche(String pathFile1,String pathFile2,String pathFile3) throws Exception{
+		List<Contatto> contatti1=new ArrayList<Contatto>();
+		
+		
+		caricaRubrica(contatti1,pathFile1);
+		caricaRubrica(contatti1,pathFile2);
+		
+		FileWriter writer = new FileWriter(new File(pathFile3),true);
+		for ( Contatto contatto : contatti1) {
+			writer.write(contatto.getCognome());
+			writer.write(';');
+			writer.write(contatto.getNome());
+			writer.write(';');
+			writer.write(contatto.getTelefono());
+			writer.write(';');
+			writer.write(contatto.getEmail());
+			writer.write('\n');
+		}
+		
+		writer.flush();
+		writer.close();
+		
+	}
+	
+	
+	public static void nameOrder(List<Contatto> contatti ) {
+		
+		Collections.sort(contatti, (c1,c2)->c1.getNome().compareTo(c2.getNome()));
+		
+		for(Contatto c : contatti) {
+			System.out.println(c.toString());
+		}
+	
+	}
+	
+	public static void cognomeOrder(List<Contatto> contatti) {
+		Collections.sort(contatti,(c1,c2)->c1.getCognome().compareTo(c2.getCognome()));
+		for(Contatto c : contatti) {
+			System.out.println(c.toString());
+		}
+	}
+	
+	public static void cercaContatto(List<Contatto> contatti, String attributo) {
+		
+		
+	}
 }
