c5a89a759dcb04d8d2cb60cf288c0c76dd8212c9
AntonyShenouda <antonyshenouda@outlook.it>
2021-04-20 16:22:25 +0200

    Add methods to mergeFile, convertToList of Conatact ..

--- src/it/beije/makemake/file/Manager.java
+++ src/it/beije/makemake/file/Manager.java
@@ -1,24 +1,100 @@
 package it.beije.makemake.file;
 import java.io.*;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 import it.beije.makemake.file.rubrica.*;
 
 public class Manager {
 	public static void main(String[] arg) throws Exception{
-		File file = new File("C:/Users/Padawan11/Desktop/rubrica1.csv");
-		ArrayList<Contatto> contatti = new ArrayList<>();
+		File fileA = new File("C:/Users/Padawan11/Desktop/rubrica1.csv");
+		File fileB = new File("C:/Users/Padawan11/Desktop/rubrica.txt");
+		File file = null;
 		
-
-		contatti = convertRubricaToList(file);
+		ArrayList<Contatto> contatti = null;
 		
-		searchForDuplicates(contatti);
-		orderRubricaNome(file);
+		file = mergeFile(fileA, fileB);
+		readFile(file);
+		searchContact(file);
+		
 		
 	}
+	public static void searchContact(File file) throws Exception {
+		Scanner in = new Scanner(System.in);
+		ArrayList<Contatto> cont = convertRubricaToList(file);
+	
+		System.out.println("Forniscimi il parametro per trovare il contatto : ");
+		boolean flag = true;
+		String value = null;
+		
+		do {
+			value = in.next();
+			
+			switch(value) {
+				case "Nome":
+				case "Cognome":
+				case "Telefono":
+				case "Email" :
+					flag = false;
+					break;
+				default:
+					System.out.println("Hai cannato il parametro forniscimene uno valido!");
+					break;
+					
+			}
+		}while(flag);
+		
+		for(Contatto c : cont) {
+			Method method = c.getClass().getMethod("get"+value, new Class[]{});
+			String returnValue = (String)method.invoke(c, null);
+			System.out.println(returnValue);
+		}
+		
+		
+	}
+	public static File mergeFile(File fileA, File fileB) throws Exception{
+		ArrayList<Contatto> listA = convertRubricaToList(fileA);
+		ArrayList<Contatto> listB = convertRubricaToList(fileB);
+		
+		File newFile = new File("C:/Users/Padawan11/Desktop/nuovaRubrica.csv");
+		FileWriter writer = new FileWriter(newFile);
+		
+		writer.write("NOME;COGNOME;TELEFONO;EMAIL;\n");
+		
+		for(Contatto contatto : listA) {
+			writer.write(contatto.getNome());
+			writer.write(';');
+			writer.write(contatto.getCognome());
+			writer.write(';');
+			writer.write(contatto.getTelefono());
+			writer.write(';');
+			writer.write(contatto.getEmail());
+			writer.write('\n');
+		}
+		
+		writer.flush();
+		for(Contatto contatto : listB) {
+			writer.write(contatto.getNome());
+			writer.write(contatto.getCognome());
+			writer.write(';');
+			writer.write(contatto.getNome());
+			writer.write(';');
+			writer.write(contatto.getTelefono());
+			writer.write(';');
+			writer.write(contatto.getEmail());
+			writer.write('\n');
+		}
+		writer.flush();
+		writer.close();
+		
+		return newFile;
+		
+	}
+	
 	public static void readFile(File file) throws Exception{
 		if( !(file.exists() || file.isFile())) {
 			System.out.println("Hai cannato qualcosa");
@@ -36,6 +112,7 @@ public class Manager {
 		bufferedReader.close();
 	}
 	
+
 	public static ArrayList<Contatto> convertRubricaToList(File file) throws Exception{
 		FileReader fileReader = new FileReader(file);
 		BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -85,4 +162,5 @@ public class Manager {
 			System.out.println(c);
 		}
 	}
+
 }
