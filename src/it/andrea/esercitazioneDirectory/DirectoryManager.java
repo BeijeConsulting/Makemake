4c602e8d13b79c0e805c5634aa0011e81bb59ea8
staibanoandrea <andreastaibano95@gmail.com>
2021-04-20 17:59:47 +0200

    Updated DirectoryManager, now writes to file

--- src/it/andrea/esercitazioneDirectory/DirectoryManager.java
+++ src/it/andrea/esercitazioneDirectory/DirectoryManager.java
@@ -1,25 +1,34 @@
 package it.andrea.esercitazioneDirectory;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 
 public class DirectoryManager {
 	public static final String path = "C:/Users/Padawan10/git/Makemake/src";
+	public static final String destPath = "C:/Users/Padawan10/Desktop/tree.txt";
 
-	public static void printTree(File file, String indent) {
-		System.out.println(indent + file.getName());
+	public static void printTree(File file, String indent, File destFile) throws Exception {
+		FileWriter fileWriter = new FileWriter(destFile, true);
+		BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+		bufferedWriter.append(indent + file.getName() + "\n");
+		bufferedWriter.flush();
 		if (file.isDirectory()) {
 			for (File currentFile : file.listFiles()) {
-				printTree(currentFile, indent + "\t");
+				printTree(currentFile, indent + "\t", destFile);
 			}
 		}
+		bufferedWriter.close();
 	}
 
-	public static void printTree(File file) {
-		printTree(file, "");
+	public static void printTree(File file, File destFile) throws Exception {
+		FileWriter fileWriter = new FileWriter(destFile);
+		printTree(file, "", destFile);
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		File file = new File(path);
-		printTree(file);
+		File destFile = new File(destPath);
+		printTree(file, destFile);
 	}
 }
