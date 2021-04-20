662813f3e3a8e1a963439e2f5a3475d249bf97b6
tommicasti <tommi.casti@gmail.com>
2021-04-20 15:07:57 +0200

    esercizi

--- src/it/beije/makemake/rubrica/Rubrica.java
+++ src/it/beije/makemake/rubrica/Rubrica.java
@@ -1,6 +1,7 @@
 package it.beije.makemake.rubrica;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -9,10 +10,13 @@ import java.util.List;
 
 public class Rubrica {
 	static ArrayList<Contatto> cont = new ArrayList<Contatto>();
-
+	static File f = new File("C:/Users/Padawan09/git/Makemake/src/it/beije/makemake/rubrica/ciao.txt");
+	static File g = new File("C:/Users/Padawan09/git/Makemake/src/it/beije/makemake/rubrica/come.txt");
+	static File h = new File("C:/Users/Padawan09/git/Makemake/src/it/beije/makemake/rubrica/merge1.txt");
 	public static void main(String[] args) throws Exception {
-		caricaContatti(cont, "C:/Users/Padawan09/git/Makemake/src/it/beije/makemake/rubrica/rubrica1.csv");
-		scriviLista("C:/Users/Padawan09/git/Makemake/src/it/beije/makemake/rubrica/rubrica1.csv","C:/Users/Padawan09/git/Makemake/src/it/beije/makemake/rubrica/contatti.txt");
+		//caricaContatti(cont, "C:/Users/Padawan09/git/Makemake/src/it/beije/makemake/rubrica/rubrica1.csv");
+		//scriviLista(f, g);
+		fusioneFile(f,g,h);
 	}
 
 	public static void caricaContatti(List<Contatto> contatti, String path) throws Exception {
@@ -29,39 +33,56 @@ public class Rubrica {
 			c.setTelefono(rowParts[2]);
 			c.setEmail(rowParts[3]);
 			contatti.add(c);
-			
+
 		}
 		for (Contatto c : contatti) {
 			System.out.println(c.toString());
 		}
 	}
-	
-	
-	
+
 	public static void scriviLista(File orig, File clone) throws Exception {
+		
 		if (!orig.exists()) {
 			System.out.println("File origine non trovato!!");
 			return;
 		}
 		if (clone.exists()) {
 			System.out.println("File destinazione già esistente!");
-			return;
+			
 		}
 
 		FileReader fileReader = new FileReader(orig);
-		BufferedReader bufferedReader = new BufferedReader(fileReader);
 		FileWriter fileWriter = new FileWriter(clone);
-		
-		while (fileReader.ready()) {
-			
-			fileWriter.write(bufferedReader.readLine());
+		int length;
+		while((length=fileReader.read())>0){
+			fileWriter.write(length);
 		}
+		
 		fileWriter.close();
 		fileReader.close();
 	}
+	
+	public static void fusioneFile(File f1,File f2, File f3) throws Exception {
+		
+		 FileReader a=new FileReader(f1);
+		 FileReader b=new FileReader(f2);	
+		 FileWriter fw1 = new FileWriter(f3);
+		
+		
+			while (a.ready()) { // ready restituisce boolean se c è carattere da leggere
+				fw1.write(a.read()); // .read prende carattere successivo
 
-	
-	
-	
+			}
+			fw1.close();
+			FileWriter fw2 = new FileWriter(f3, true); // con il true appende il risultato
 
-}
+			while (b.ready()) { // ready restituisce boolean se c è carattere da leggere
+				fw2.write(b.read()); // .read prende carattere successivo
+
+			}
+			fw2.close();
+			a.close();
+			b.close();
+		}
+
+	}
