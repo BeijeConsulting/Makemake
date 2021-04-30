ebd59c92282179202e8cabdaaadd1f2efe7f9810
tyra <tyramariedeclaro@gmail.com>
2021-04-30 17:17:31 +0200

    util per Rubrica Digitale

--- src/it/beije/makemake/database/HDBSingleton.java
+++ src/it/beije/makemake/database/HDBSingleton.java
@@ -5,6 +5,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import rubrica.Contatto;
+
 public class HDBSingleton {
 	private static HDBSingleton instance;
 	private Configuration configuration;
@@ -16,7 +18,7 @@ public class HDBSingleton {
 	public static HDBSingleton getHDBSingleton() {
 		if (instance == null) {
 			instance = new HDBSingleton();
-			instance.configuration = new Configuration().configure();
+			instance.configuration = new Configuration().configure().addAnnotatedClass(Contatto.class);
 			instance.sessionFactory = instance.configuration.buildSessionFactory();
 		}
 		return instance;
