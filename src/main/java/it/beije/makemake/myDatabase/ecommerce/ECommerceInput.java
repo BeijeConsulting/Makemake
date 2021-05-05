1fa152535ae0c57e2560f4cea8f627b1eaf3cae9
Korpsvart <riccardodibella9@gmail.com>
2021-05-05 14:06:39 +0200

    iniziato a implementare classe per interfaccia a linea di comando con il database ecommerce

---
+++ src/main/java/it/beije/makemake/myDatabase/ecommerce/ECommerceInput.java
@@ -0,0 +1,28 @@
+package it.beije.makemake.myDatabase.ecommerce;
+
+import java.util.Scanner;
+
+public class ECommerceInput {
+
+    Scanner sc = new Scanner(System.in);
+    private static final String[] options = {
+            "Elenco prodotti", "Elenco utenti", "Dettagli ordine"
+    };
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public void showUI() {
+        String input = "";
+        do {
+            
+            
+        } while(!input.equals("exit"));
+    }
+
+
+
+
+}
